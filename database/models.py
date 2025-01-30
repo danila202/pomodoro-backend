d1a99083b817440c22292bff21eb30b5b3d16b17
Danila Dyakonov <danila.dyakonov@raiffeisen.ru>
2025-01-30 23:13:43 +0300

    init

--- database/models.py
+++ database/models.py
@@ -1,31 +1,29 @@
-from typing import Any, Optional
+from typing import Any, Optional, Annotated
 
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, declared_attr
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
+
+intpk= Annotated[int, mapped_column(primary_key=True, nullable=False)]
 
 class Base(DeclarativeBase):
-    id: Any
-    __name__: str
 
     __allow_unmapped__ = True
 
-    @declared_attr
-    def __tablename__(self) -> str:
-        return self.__name__.lower()
+
 
 
 class Tasks(Base):
-    __tablename__ = "Tasks"
+    __tablename__ = "tasks"
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[intpk]
     name: Mapped[str]
     pomodoro_count: Mapped[int]
     category_id: Mapped[int] = mapped_column(nullable=False)
 
 
 class Categories(Base):
-    __tablename__ = "Categories"
+    __tablename__ = "categories"
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[intpk]
     type: Mapped[Optional[str]]
     name: Mapped[str]
--- dependecy.py
+++ dependecy.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 
 from database import get_db_session
-from cache import get_redis_connection
-from repository import TaskRepository, TaskCache
+
+from repository import TaskRepository
 from service import TaskService
 
 
@@ -11,16 +11,10 @@ def get_tasks_repository() -> TaskRepository:
     return TaskRepository(db_session)
 
 
-def get_tasks_cache_repository() -> TaskCache:
-    redis_connection = get_redis_connection()
-    return TaskCache(redis_connection)
-
-
 def get_task_service(
     task_repository: TaskRepository = Depends(get_tasks_repository),
-    task_cache: TaskCache = Depends(get_tasks_cache_repository)
 ) -> TaskService:
     return TaskService(
         task_repository=task_repository,
-        task_cache=task_cache
+
     )
